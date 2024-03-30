099454988f5e3e7ad0c7dc6188149cc5eaf46730
LuizGuilherme13 <luizguilherme.bs13@hotmail.com>
2024-03-30 18:54:36 -0300

    first commit

---
+++ public/vars.php
@@ -0,0 +1,16 @@
+<?php
+$title = 'Learning PHP';
+$greeting = 'Hello, World!';
+$val = 100;
+
+// echo gettype($title);
+// echo gettype(100);
+// echo gettype(10.33);
+// echo gettype(false);
+// echo gettype(['', '']);
+// class Test
+// {
+// }
+
+// echo gettype(new Test);
+// echo gettype(null);
